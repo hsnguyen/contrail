04323d2251c033b340add61811f06ba8e7f3650d
Jeremy Lewi <jeremy@lewi.us>
2012-12-29 21:21:24 -0800

    Created SortAndBuildIndex

--- src/main/java/contrail/tools/CreateGraphIndex.java
+++ src/main/java/contrail/tools/CreateGraphIndex.java
@@ -1,3 +1,17 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+// Author: Jeremy Lewi(jeremy@lewi.us)
 package contrail.tools;
 
 import java.io.IOException;
@@ -8,9 +22,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeSet;
 
-import org.apache.avro.hadoop.file.SortedKeyValueFile;
 import org.apache.avro.Schema;
 import org.apache.avro.file.DataFileStream;
+import org.apache.avro.hadoop.file.SortedKeyValueFile;
 import org.apache.avro.specific.SpecificDatumReader;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
