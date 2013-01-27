f87e22e1a1e5cf4d433e64395529e79c0121116a
Jeremy Lewi <jeremy@lewi.us>
2013-01-26 19:17:29 -0800

    Code for ReverseReads is written. Need to add a unittest.

--- src/main/java/contrail/stages/ReverseReads.java
+++ src/main/java/contrail/stages/ReverseReads.java
@@ -19,7 +19,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.avro.mapred.AvroWrapper;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
@@ -37,10 +37,11 @@ import org.apache.hadoop.mapred.RunningJob;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.Logger;
 
-import contrail.correct.FastQToAvro;
 import contrail.io.FastQInputFormat;
 import contrail.io.FastQText;
-import contrail.sequences.FastQRecord;
+import contrail.sequences.DNAAlphabetFactory;
+import contrail.sequences.DNAUtil;
+import contrail.sequences.Sequence;
 
 /**
  * Mapper only job to reverse the reads in a FastQ file.
@@ -64,40 +65,45 @@ public class ReverseReads extends Stage {
 
   public static class ReverseMapper extends MapReduceBase
       implements Mapper<LongWritable, FastQText, FastQText, NullWritable>{
-    private FastQRecord read = new FastQRecord();
-    private AvroWrapper<FastQRecord> out_wrapper = new AvroWrapper<FastQRecord>(read);
+    private Sequence sequence;
+    private String qValue;
 
     public void configure(JobConf job) {
-      Configuration conf = new Configuration();
-      FileSystem hdfs;
+      sequence = new Sequence(DNAAlphabetFactory.create());
     }
 
     public void map(LongWritable line, FastQText record,
-        OutputCollector<AvroWrapper<FastQRecord>, NullWritable> output, Reporter reporter)
+        OutputCollector<FastQText, NullWritable> collector, Reporter reporter)
             throws IOException {
+      // Get the reverse complement of the sequence.
+      sequence.readCharSequence(record.getDna());
+      sequence = DNAUtil.reverseComplement(sequence);
 
-      read.id = record.getId();
-      read.read = record.getDna();
-      read.qvalue = record.getQValue();
+      // Reverse the qValue
+      qValue = record.getQValue();
+      StringUtils.reverse(qValue);
 
-      output.collect(out_wrapper, NullWritable.get());
+      record.set(record.getId(), sequence.toString(), qValue);
+
+      collector.collect(record, NullWritable.get());
     }
   }
 
   public RunningJob runJob() throws Exception {
-
-    JobConf conf = new JobConf(FastQToAvro.class);
-
-    conf.setJobName("FastQToAvro");
-    String inputPath, outputPath, datatype;
-    Long splitSize;
-    conf.setJobName("Rekey Data");
-    String[] required_args = {"inputpath", "outputpath","splitSize"};
+    String[] required_args = {"inputpath", "outputpath"};
     checkHasParametersOrDie(required_args);
 
-    inputPath = (String) stage_options.get("inputpath");
-    outputPath = (String) stage_options.get("outputpath");
-    splitSize = (Long) stage_options.get("splitSize");
+    String inputPath = (String) stage_options.get("inputpath");
+    String outputPath = (String) stage_options.get("outputpath");
+
+    Configuration base_conf = getConf();
+    JobConf conf = null;
+    if (base_conf != null) {
+      conf = new JobConf(getConf(), this.getClass());
+    } else {
+      conf = new JobConf(this.getClass());
+    }
+    conf.setJobName(this.getClass().getName());
 
     //Sets the parameters in JobConf
     initializeJobConfiguration(conf);
@@ -105,9 +111,9 @@ public class ReverseReads extends Stage {
     FileOutputFormat.setOutputPath(conf, new Path(outputPath));
 
     // Input
-    conf.setMapperClass(FastQToAvroMapper.class);
+    conf.setMapperClass(ReverseMapper.class);
     conf.setInputFormat(FastQInputFormat.class);
-    conf.setLong("FastQInputFormat.splitSize", splitSize);
+
     //Map Only Job
     conf.setNumReduceTasks(0);
 
@@ -125,253 +131,9 @@ public class ReverseReads extends Stage {
     return result;
   }
 
-    /**
-     * Mapper.
-     */
-//    public static class FastqPreprocessorMapper extends MapReduceBase
-//    implements Mapper<LongWritable, Text, AvroWrapper<CompressedRead>, NullWritable>
-//    {
-//      private int idx = 0;
-//
-//      private String filename = null;
-//
-//      private int mate_id = 0x0;
-//
-//      /**
-//       *  The alphabet for encoding the sequences.
-//       */
-//      private Alphabet alphabet;
-//
-//      private String counter = "pair_unknown";
-//
-//      private CompressedRead read = new CompressedRead();
-//      private AvroWrapper<CompressedRead> out_wrapper =
-//          new AvroWrapper<CompressedRead>(read);
-//
-//      private ByteReplaceAll replacer = null;
-//
-//      // The byte value to replace multi-byte characters with
-//      // this an underscore.
-//      public final byte MULTIBYTE_REPLACE_VALUE = 0x5f;
-//
-//      // The sequence.
-//      private Sequence sequence;
-//
-//      // An array which can be used to tell if a UTF8 value
-//      // is whitespace
-//      private boolean[] utf8_whitespace;
-//
-//      // Store the utf8 byte values of various characters
-//      private byte utf8_at;
-//      private byte utf8_space;
-//
-//      public void configure(JobConf job)
-//      {
-//        filename = job.get("map.input.file");
-//
-//        boolean usesuffix = Integer.parseInt(job.get("PREPROCESS_SUFFIX")) == 1;
-//
-//        String suffix = null;
-//        if (usesuffix)
-//        {
-//          if  (filename.contains("_1.")) {
-//            suffix = "_1";
-//            mate_id = 0x1;
-//            counter = "pair_1";
-//          }
-//          else if (filename.contains("_2.")) {
-//            suffix = "_2";
-//            mate_id = 0x2;
-//            counter = "pair_2";
-//          }
-//          else {
-//            counter = "pair_unpaired";
-//          }
-//
-//          System.err.println(filename + " suffix: \"" + suffix + "" + "\"");
-//        }
-//        read.mate_pair_id = mate_id;
-//        replacer = new ByteReplaceAll(":#-.|/$%&'()*+,-./:","_");
-//
-//        alphabet = DNAAlphabetFactory.create();
-//        sequence = new Sequence(alphabet);
-//
-//        // utf8_whitespace[x] = True for the utf8 charater with value
-//        // x, if x is a white space character. False otherwise.
-//        utf8_whitespace = new boolean[255];
-//        java.util.Arrays.fill(utf8_whitespace, false);
-//
-//        String white_space = " \n\t";
-//        byte[] white_space_bytes = ByteUtil.stringToBytes(white_space);
-//
-//        for (int pos = 0; pos < white_space_bytes.length; pos++) {
-//          utf8_whitespace[pos] = true;
-//        }
-//
-//        utf8_at = ByteUtil.stringToBytes("@")[0];
-//        utf8_space = ByteUtil.stringToBytes(" ")[0];
-//      }
-//
-//      public void map(LongWritable lineid, Text line,
-//          OutputCollector<AvroWrapper<CompressedRead>, NullWritable> output, Reporter reporter)
-//              throws IOException
-//              {
-//        if (idx == 0)
-//        {
-//          // We operate on the bytes instead of converting to a string.
-//          // The advantage is that we can use our more efficient implementation
-//          // for replace all.
-//          byte[] data = line.getBytes();
-//
-//          // Replace any multibyte characters with "_"
-//          int valid_length = ByteUtil.replaceMultiByteChars(
-//              data, MULTIBYTE_REPLACE_VALUE, line.getLength());
-//
-//          // make sure it starts with the @ symbol
-//          if (data[0] != utf8_at)
-//          {
-//            throw new IOException("ERROR: Invalid readname: " + line.toString() + " in " + filename);
-//          }
-//
-//          // Find the location of the first space in the name.
-//          int end_index = valid_length-1;
-//          for (int index = 1; index <= end_index; index++){
-//            if (data[index] == utf8_space){
-//              end_index = index -1;
-//              break;
-//            }
-//          }
-//
-//          // Remove any trailing whitespace.
-//          while (utf8_whitespace[ByteUtil.byteToUint(data[end_index])]) {
-//            end_index--;
-//          }
-//
-//          // Remove the leading '@' and chop everything after the first space.
-//          data = java.util.Arrays.copyOfRange(data, 1, end_index+1);
-//
-//          // Replace any funny characters.
-//          replacer.replaceAll(data);
-//
-//          read.setId(new String(data, ByteReplaceAll.encoding));
-//        }
-//        else if (idx == 1) {
-//          byte[] raw_bytes = line.getBytes();
-//          // TODO(jeremy@lewi.us): We should really only be checking the bytes
-//          // up to line.getLength()
-//          if (ByteUtil.hasMultiByteChars(raw_bytes)){
-//            throw new RuntimeException("DNA sequence contained illegal characters. Sequence is: " + line.toString());
-//          }
-//
-//          sequence.readUTF8(raw_bytes, line.getLength());
-//          int num_bytes =  (int)Math.ceil((alphabet.bitsPerLetter() * sequence.size())/ 8.0);
-//
-//          read.setDna(ByteBuffer.wrap(sequence.toPackedBytes(), 0, num_bytes));
-//          read.setLength(line.getLength());
-//        }
-//        else if (idx == 2) {
-//        }
-//        else if (idx == 3)
-//        {
-//          output.collect(out_wrapper, NullWritable.get());
-//
-//          reporter.incrCounter("Contrail", "preprocessed_reads", 1);
-//          reporter.incrCounter("Contrail", counter, 1);
-//        }
-//
-//        idx = (idx + 1) % 4;
-//      }
-//
-//      public void close() throws IOException {
-//        if (idx != 0)
-//        {
-//          throw new IOException("ERROR: closing with idx = " + idx + " in " + filename);
-//        }
-//      }
-//    }
-//
-//    @Override
-//    public RunningJob runJob() throws Exception {
-//      sLogger.info("Tool name: FastqPreprocessorAvroCompressed");
-//      String inputPath = (String) stage_options.get("inputpath");
-//      String outputPath = (String) stage_options.get("outputpath");
-//      sLogger.info(" - input: "  + inputPath);
-//      sLogger.info(" - output: " + outputPath);
-//
-//      Configuration base_conf = getConf();
-//      JobConf conf = null;
-//      if (base_conf != null) {
-//        conf = new JobConf(getConf(), this.getClass());
-//      } else {
-//        conf = new JobConf(this.getClass());
-//      }
-//
-//      sLogger.info("mapred.map.tasks=" + conf.get("mapred.map.tasks", ""));
-//      conf.setJobName("FastqPreprocessorAvroCompressed " + inputPath);
-//
-//      // Stage specific configuration options.
-//      conf.setLong("PREPROCESS_SUFFIX", 1);
-//
-//      FileInputFormat.addInputPath(conf, new Path(inputPath));
-//      FileOutputFormat.setOutputPath(conf, new Path(outputPath));
-//
-//      conf.setInputFormat(TextInputFormat.class);
-//
-//      conf.setMapOutputKeyClass(Text.class);
-//      conf.setMapOutputValueClass(Text.class);
-//
-//      conf.setMapperClass(FastqPreprocessorMapper.class);
-//
-//      conf.setInputFormat(NLineInputFormat.class);
-//      conf.setInt("mapred.line.input.format.linespermap", 2000000); // must be a multiple of 4
-//
-//      // This is a mapper only job.
-//      conf.setNumReduceTasks(0);
-//
-//      // TODO(jlewi): use setoutput codec to set the compression codec.
-//      AvroJob.setOutputSchema(conf,new CompressedRead().getSchema());
-//
-//      if (stage_options.containsKey("writeconfig")) {
-//        writeJobConfig(conf);
-//      } else {
-//        //delete the output directory if it exists already
-//        FileSystem.get(conf).delete(new Path(outputPath), true);
-//
-//        long start_time = System.currentTimeMillis();
-//        RunningJob job = JobClient.runJob(conf);
-//        long numReads =
-//            job.getCounters().findCounter(
-//                "org.apache.hadoop.mapred.Task$Counter",
-//                "MAP_OUTPUT_RECORDS").getValue();
-//        sLogger.info("Number of reads:" + numReads);
-//        long end_time = System.currentTimeMillis();
-//        double nseconds = (end_time - start_time) / 1000.0;
-//        sLogger.info("Job took: " + nseconds + " seconds");
-//        return job;
-//      }
-//      return null;
-//    }
-//
-//    /**
-//     * Get the options required by this stage.
-//     */
-//    protected Map<String, ParameterDefinition> createParameterDefinitions() {
-//      HashMap<String, ParameterDefinition> defs =
-//          new HashMap<String, ParameterDefinition>();
-//
-//      defs.putAll(super.createParameterDefinitions());
-//
-//      for (ParameterDefinition def:
-//        ContrailParameters.getInputOutputPathOptions()) {
-//        defs.put(def.getName(), def);
-//      }
-//      return Collections.unmodifiableMap(defs);
-//    }
-//
-
-    public static void main(String[] args) throws Exception {
-      int res = ToolRunner.run(
-          new Configuration(), new ReverseReads(), args);
-      System.exit(res);
-    }
+  public static void main(String[] args) throws Exception {
+    int res = ToolRunner.run(
+        new Configuration(), new ReverseReads(), args);
+    System.exit(res);
+  }
 }
